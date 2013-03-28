d9c65157455bbc2188e75f891594092e281f7df4
Viktar Basharymau <Viktar.Basharymau@gmail.com>
2013-03-28 22:32:55 +0300

    Added tests for ResellerContact

---
+++ spec/lib/mailroute/models/reseller_contact_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Mailroute::ResellerContact, :vcr => true do
+  before { configure_mailroute }
+
+  let(:reseller) { Mailroute::Reseller.get(4) }
+
+  describe 'CRUD' do
+    it 'should create, read, update and delete reseller contacts' do
+      contact = Mailroute::ResellerContact.create(email: 'haskell@example.com', reseller: reseller)
+
+      contact.id.should be
+
+      contact2 = Mailroute::ResellerContact.get(contact.id)
+
+      contact2.should == contact
+
+      contact2.city = 'Minsk'
+      contact2.save!
+
+      contact2.reseller.should == reseller
+
+      contact.reload.city.should == 'Minsk'
+
+      contact.delete
+
+      expect { contact2.reload }.to raise_error ActiveResource::ResourceNotFound
+
+      reseller.contacts.should_not include contact
+    end
+  end
+end
