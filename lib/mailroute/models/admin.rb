24378aadad441be6f37111cfc508be3f0f5d7fb7
Viktar Basharymau <viktar.basharymau@gmail.com>
2013-05-03 00:06:01 +0300

    Implemented Admin#regenerate_api_key
    Closes #3

--- lib/mailroute/models/admin.rb
+++ lib/mailroute/models/admin.rb
@@ -17,5 +17,44 @@ module Mailroute
       prefix_options, query_options = split_options(prefix_options) if query_options.nil?
       "#{prefix(prefix_options)}admins/#{scope[:name]}/#{scope[:id]}/admin/#{URI.parser.escape id.to_s}/#{query_string(query_options)}"
     end
+
+    def element_path
+      super(scope)
+    end
+
+    def regenerate_api_key
+      response = connection.post(element_path + 'regenerate_api_key/', nil, self.class.headers)
+      self.class.format.decode(response.body)['api_key']
+    end
+
+    def scope
+      raise 'invalid state' unless resource_uri =~ /admins\/([^\/]+)\/(\d+)\/admin/
+      { :scope => { :id => $2.to_i, :name => $1 } }
+    end
+
+    def customer
+      if resource_uri =~ /admins\/customer\/(\d+)\/admin/
+        @customer ||= Mailroute::Customer.get($1.to_i)
+      else
+        nil
+      end
+    end
+
+    def reseller
+      if resource_uri =~ /admins\/reseller\/(\d+)\/admin/
+        @reseller ||= Mailroute::Reseller.get($1.to_i)
+      else
+        nil
+      end
+    end
+
+    def reload
+      @customer = @reseller = nil
+      super
+    end
+
+    def self.get(id, scope)
+      find(id, :params => { :scope => { :name => scope.keys.first, :id => scope.values.first } })
+    end
   end
 end
--- spec/lib/mailroute/models/admin_spec.rb
+++ spec/lib/mailroute/models/admin_spec.rb
@@ -10,4 +10,32 @@ describe Mailroute::Admin, :vcr => true do
       admins.count.should == 3
     end
   end
+
+  describe '#get' do
+    context 'getting customer admin' do
+      subject(:admin) { Mailroute::Admin.get(9252, :customer => 11372) }
+
+      its(:id) { should == 9252 }
+      its(:email) { should == 'admin@example.com' }
+      its(:customer) { should be_a Mailroute::Customer }
+    end
+  end
+
+  describe '#regenerate_api_key' do
+    context 'an admin' do
+      let(:admin) { Mailroute::Admin.get(9252, :customer => 11372) }
+
+      context 'regenerating api key' do
+        subject(:new_key) { admin.regenerate_api_key }
+
+        it { should match /\A[0-9a-f]{40}\Z/ }
+
+        context 'regenerating api key once again' do
+          let(:another_key) { admin.regenerate_api_key }
+
+          it { should_not == another_key }
+        end
+      end
+    end
+  end
 end
